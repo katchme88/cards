c3f4a144c42e5ef63af842a262b559ede1e1f614
Talha <engr.jerrykay@yahoo.ca>
2018-01-14 19:57:13 -0600

    senior selection rule completed

--- gameplay/rules.js
+++ gameplay/rules.js
@@ -2,36 +2,12 @@ exports.getSenior = function (data, trumpRevealed, trumpSuit, revealedInThis) {
 
     currentRoundCards = data.slice();
     trumpExist = 0;
-
-    if (!revealedInThis) {
-        
-        if (!trumpRevealed) {
-            currentSuit = data[0].charAt(0);
-        } else {
-            for (var i = 0; i < data.length; i++) {
-                if (data[i].charAt(0) == trumpSuit) {
-                    trumpExist = 1;
-                    break;
-                }
-            }
-            
-            if (trumpExist) {
-                currentSuit = trumpSuit;
-            } else {
-                currentSuit = data[0].charAt(0);
-            }
-        }
-
-        for (var i = 0; i < data.length; i++) {
-            if (data[i].charAt(0) != currentSuit) {
-                data[i] = '0';
-            }
-        }
-        console.log(currentRoundCards.length)
-        console.log(currentRoundCards);
-        seniorCard = data.sort()[data.length -1];
-        return ([seniorCard, currentRoundCards.indexOf(seniorCard)+1]);
+       
+    if (!trumpRevealed) {
+        // If trump is not yet revealed the first card stays the current suit
+        currentSuit = data[0].charAt(0);
     } else {
+        //if trum is revealed  check for the trump suit in the current round
         for (var i = 0; i < data.length; i++) {
             if (data[i].charAt(0) == trumpSuit) {
                 trumpExist = 1;
@@ -39,10 +15,29 @@ exports.getSenior = function (data, trumpRevealed, trumpSuit, revealedInThis) {
             }
         }
         
-        if (trumpExist) {
+        if (trumpExist && !revealedInThis) {
+            // If trump exist but not revealed in this set trump as the current suit
             currentSuit = trumpSuit;
-        } else {
+        } else if (trumpExist && revealedInThis) {
+            // If trump exist and also revealed in this then neglect prior cards
+            currentSuit = trumpSuit;
+            for (var i = 0; i < revealedInThis; i++) {
+                data[i] = '0';
+            }
+        }
+        else {
+            // If trump rnot exist but is revealed then treat normally
             currentSuit = data[0].charAt(0);
         }
     }
+
+    // set all cards to zero which do not match the current suit
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].charAt(0) != currentSuit) {
+            data[i] = '0';
+        }
+    }
+    console.log(currentRoundCards);
+    seniorCard = data.sort()[data.length -1];
+    return ([seniorCard, currentRoundCards.indexOf(seniorCard)+1]);
 }
